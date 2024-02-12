15a8b553393ab15058aa8c7b21c24ee3c4adac66
Murilo Araujo <murilo@muriloaraujo.com>
2024-02-12 15:55:05 -0300

    Initial Commit

---
+++ StoreIt/StoreItApp.swift
@@ -0,0 +1,32 @@
+//
+//  StoreItApp.swift
+//  StoreIt
+//
+//  Created by Murilo Araujo on 12/02/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StoreItApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
